6ecd6112cb14d65415b3de88bacc8321ffebfda7
Jesse <nguyenjessev@gmail.com>
2020-08-14 16:51:48 -0400

    Add legislators_by_zipcode method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,18 +1,39 @@
 # frozen-string-literal: true
 
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyA2gf1HONoNfuNKAU0917tx5EBRhN3ESQ8'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+    legislators = legislators.officials
+
+    legislator_names = legislators.map(&:name)
+
+    legislator_names.join(', ')
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
 puts 'EventManager Initialized!'
 
-contents = CSV.open('event_attendees.csv', { headers: true,
-                                             header_converters: :symbol })
+contents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode}"
+  puts "#{name} #{zipcode} #{legislators}"
 end
